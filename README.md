0a7503e559519fc1cadcf472d25d79974f82a68c
Youhoseong <h0song@naver.com>
2021-05-16 21:35:37 +0900

    fix: Code factor using  spring framework

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-## NarcissismSpringBoot
+## SpringBoot
 
 - Study with cooool man
-
+- Hoseong You
+  
+  
 ### Topics
 
 - SOLID
 - DIP
 - OCP
 - App Config Refactoring
-
-
+- SRP
+- IoC / DI Container
 
--- SpringCorePrinciple/src/main/java/hello/core/AppConfig.java
+++ SpringCorePrinciple/src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // change to spring
 public class AppConfig {
 
-    public MemberService memberService(){ // 생성자 주
+    // @Bean이 붙은 모든 메서드를 호출해서 반환된 객체를 스프링 컨테이너에 등록함
+    // @Bean("name") name설정 가능, default 따르는게 좋음
+
+   @Bean // change to spring
+    public MemberService memberService(){ // spring container에 기본적으로 method 이름으로 등록
+        // key: method name , value return value
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
